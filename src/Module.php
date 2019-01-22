cc775b84291069073b08aca2ea95c52a57b02553
Curzio Della Santa <curzio.della.santa@detailnet.ch>
2019-01-22 15:48:19 +0100

    Removed autoloader

--- src/Module.php
+++ src/Module.php
@@ -2,29 +2,11 @@
 
 namespace Detail\AlgoliaSearch;
 
-use Zend\Loader\AutoloaderFactory;
-use Zend\Loader\StandardAutoloader;
-use Zend\ModuleManager\Feature\AutoloaderProviderInterface;
 use Zend\ModuleManager\Feature\ConfigProviderInterface;
 
 class Module implements
-    AutoloaderProviderInterface,
     ConfigProviderInterface
 {
-    /**
-     * {@inheritdoc}
-     */
-    public function getAutoloaderConfig()
-    {
-        return [
-            AutoloaderFactory::STANDARD_AUTOLOADER => [
-                StandardAutoloader::LOAD_NS => [
-                    __NAMESPACE__ => __DIR__,
-                ],
-            ],
-        ];
-    }
-
     /**
      * {@inheritdoc}
      */
--- tests/ModuleTest.php
+++ tests/ModuleTest.php
@@ -4,8 +4,6 @@ namespace DetailTest\AlgoliaSearch;
 
 use PHPUnit\Framework\TestCase;
 
-use Zend\Loader\StandardAutoloader;
-
 use Detail\AlgoliaSearch\Module;
 
 class ModuleTest extends TestCase
@@ -18,17 +16,6 @@ class ModuleTest extends TestCase
     protected function setUp()
     {
         $this->module = new Module();
-    }
-
-    public function testModuleProvidesAutoloaderConfig()
-    {
-        $config = $this->module->getAutoloaderConfig();
-
-        $this->assertTrue(is_array($config));
-
-        $this->assertArrayHasKey(StandardAutoloader::CLASS, $config);
-        $this->assertArrayHasKey('namespaces', $config[StandardAutoloader::CLASS]);
-        $this->assertArrayHasKey('Detail\AlgoliaSearch', $config[StandardAutoloader::CLASS]['namespaces']);
     }
 
     public function testModuleProvidesConfig()
